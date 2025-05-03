b44ab9d1b54764d9f26047aacb91209527c8c328
JimmyBares <Jimmy.Bares04@gmail.com>
2025-05-03 16:28:08 -0400

    JavaScript-1

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-let str = 'Jimmy'
-console.log(str[str.length - 1])
-console.log('Hello')
+let cash = 10
+let price = 40
+
+if (cash > price) {
+    console.log('you paid extra - here ${cash - price} dollars change')
+}
+else if (cash === price) {
+    console.log("you paid the exact amount, have a nice day")
+}
+else {
+    console.log('not enough money - you still owe ${price - cash} dollars')
+}
