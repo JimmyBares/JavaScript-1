34f2122998cda09b4ff9d08d4ae37a0b658df095
JimmyBares <Jimmy.Bares04@gmail.com>
2025-05-06 13:35:43 -0400

    JavaScript-1

--- app.js
+++
@@ -1,7 +0,0 @@
-let cash = 50
-let price = 40
-let isStoreOpen = true
-
-let str = cash >= price && isStoreOpen ? 'give receipt' : 'do not give receipt'
-console.log(str)
-// 'give receipt' : 'do not give receipt'
