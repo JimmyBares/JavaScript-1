b4a69bef103bb64f2501bdac31b4fe1d6cda7a43
JimmyBares <Jimmy.Bares04@gmail.com>
2025-05-09 12:47:02 -0400

    JavaScript-1

---
+++ app.js
@@ -0,0 +1,11 @@
+// function convertCelsiusToFahrenheit(celsius) {
+//  return celsius * 1.8 +32
+//}
+
+const convertCelsiusToFahrenheit = (celsius) => {
+    return celsius * 1.8 + 32
+}
+
+console.log(convertCelsiusToFahrenheit(0))
+console.log(convertCelsiusToFahrenheit(10))
+console.log(convertCelsiusToFahrenheit(30))
