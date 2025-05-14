0cb5269097d166680f94d6107ca4b80d3a75bbd7
JimmyBares <Jimmy.Bares04@gmail.com>
2025-05-14 14:27:16 -0400

    JavaScript-1

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-// function convertCelsiusToFahrenheit(celsius) {
-//  return celsius * 1.8 +32
-//}
+/**
+ * Turn each element in an array of dollars into cents
+ * without using the map method
+ * 
+ * @examples
+ * [1, 5, 10, 3] => [100, 500, 1000, 300]
+ * [0, 10, 20] => [0, 1000, 2000]
+ */
 
-const convertCelsiusToFahrenheit = (celsius) => {
-    return celsius * 1.8 + 32
+let dollars = [1, 5, 10, 3]
+
+// Creat a new empty array
+let cents = []
+
+// loop over every element in 'dollars'
+
+for (let i = 0; i < dollars.length; ++i) {
+    console.log(dollars[i] * 100);
+    cents.push(dollars[i] * 100);
 }
 
-console.log(convertCelsiusToFahrenheit(0))
-console.log(convertCelsiusToFahrenheit(10))
-console.log(convertCelsiusToFahrenheit(30))
+console.log(cents)
+
