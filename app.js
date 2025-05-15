a2ff4ea752c12cdb318aa489332e5e17624c0caf
JimmyBares <Jimmy.Bares04@gmail.com>
2025-05-15 17:41:37 -0400

    JavaScript-1

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-/**
- * Turn each element in an array of dollars into cents
- * without using the map method
- * 
- * @examples
- * [1, 5, 10, 3] => [100, 500, 1000, 300]
- * [0, 10, 20] => [0, 1000, 2000]
- */
-
-let dollars = [1, 5, 10, 3]
-
-// Creat a new empty array
-let cents = []
-
-// loop over every element in 'dollars'
-
-for (let i = 0; i < dollars.length; ++i) {
-    console.log(dollars[i] * 100);
-    cents.push(dollars[i] * 100);
+function toggleDarkMode() {
+document.querySelector('body').classList.toggle("dark-theme")
 }
-
-console.log(cents)
-
