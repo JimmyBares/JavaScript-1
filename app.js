4e7568f24af26c41c18b6c89fac60a9363ed6b22
JimmyBares <Jimmy.Bares04@gmail.com>
2025-05-23 14:30:24 -0400

    JavaScript-1

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const emailRef = document.querySelector(".email");
 
 // 1. Then
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-return response.json()
-}).then(data => {
-    console.log(data)
-})
+//fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+//return response.json();
+//})
+//.then(data => {
+//    console.log(data);
+//    emailRef.innerHTML
+//nev});
 
 
 // 2. Async/Await
+await fetch("https://jsonplaceholder.typicode.com/users/1")
