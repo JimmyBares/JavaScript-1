b83fc08abb94cde87c131751bcc1025a3a9161a3
JimmyBares <Jimmy.Bares04@gmail.com>
2025-05-29 13:48:09 -0400

    JavaScript-1

--- app.js
+++ app.js
@@ -1,32 +1,5 @@
-/** 
- * 2. Accept a paramenter called 'subscriptionsStatus'
- * 3. Return a new Promis inside of the function that:
- *  - if "VIP" resolve("show video")
- * - ir ""FREE" resolve("show trailer")
- *  - otherwise reject("no video")
- * 4. console.log the result of getVideo(status) in main()
- */
-
-
-function getVideo (subscriptionStatus) {
-return new Promise((resolve, reject) => {
-if (subscriptionStatus === "VIP") {
-        resolve("show video");
-    }
-
- else if (subscriptionStatus === "FREE") {
-    resolve("show Trailer")
-}
-else {
-    reject("no video")
-}
-})
+ function calcPerimeter(length, width) {
+  return 2 * (length + width);
 }
 
-async function main() {
-    const status = await getSubscriptionStatus();
-    statusRef.innerHTML = status;
-    console.log(await getVideo(status))
-}
-
-main();
+console.log(calcPerimeter(20, 10));
