60e7cf45402b6338acb328fb18656726ac5ea6cd
JimmyBares <Jimmy.Bares04@gmail.com>
2025-06-04 12:58:37 -0400

    JavaScript-1

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
- function isLoggedinAndSubscribed(loggedIn, subscribed) {
- return (loggedIn === 'LOGGED_IN')  ||  (subscribed === 'SUBSCRIBED');
+ function arrSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; ++i) {
+        // The for statement above and console.log below shows how to loop through every element in an array//
+        sum = sum + arr[i];
+              console.log(arr[i]);
+    }
+    return sum;
+    }
 
-}
-console.log(isLoggedinAndSubscribed('LOGGED_OUT', 'UNSUBSCRIBED')) 
+console.log(arrSum([ 2, 2, 2]));
