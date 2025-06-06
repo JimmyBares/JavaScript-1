36be793b2fbac4df457955f3699e75bb9321fc21
JimmyBares <Jimmy.Bares04@gmail.com>
2025-06-06 14:45:36 -0400

    JavaScript-1

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
- function arrSum(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; ++i) {
-        // The for statement above and console.log below shows how to loop through every element in an array//
-        sum = sum + arr[i];
-              console.log(arr[i]);
-    }
-    return sum;
-    }
+ function calcTime(seconds) {
+let timerMinutes = Math.floor(seconds / 60);
+let timerSeconds = seconds  % 60;
 
-console.log(arrSum([ 2, 2, 2]));
+ if(timerMinutes.toString(). length === 1) {
+     timerMinutes = '0' + timerMinutes 
+ }
+
+return timerMinutes + ':' + timerSeconds;
+}
+
+console.log(calcTime(70));
